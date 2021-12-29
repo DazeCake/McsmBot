6b730d7054c616ce130e542dfb1ee0f63e185b8c
DazeCake <DazeCake@gmail.com>
2021-12-29 15:16:32 +0800

    More Kotlin Way

--- src/main/kotlin/api/McsmRequests.kt
+++ src/main/kotlin/api/McsmRequests.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.runBlocking
 import moe.dazecake.McsmBot
 import moe.dazecake.McsmBotConfig
 import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.utils.info
 import net.mamoe.mirai.utils.warning
 import okhttp3.*
 import java.io.IOException
@@ -12,37 +13,7 @@ import java.io.IOException
 object McsmRequests {
     private val config = McsmBotConfig
 
-    fun getStatus(group: Group) {
-
-        val request = Request.Builder()
-            .url("${config.McsmIP}api/status/${config.ServerName}")
-            .build()
-
-        OkHttpClient().newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                McsmBot.logger.warning { "出现网络问题,请求失败!" }
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-
-                val res = Gson().fromJson(response.body?.string(), hashMapOf<String, String>().javaClass)
-                runBlocking {
-                    println(res.toString())
-                    group.sendMessage(
-                        """
-                        名称: ${res["id"]}
-                        版本: ${res["version"]}
-                        在线人数: ${res["current_players"]}/${res["max_players"]}
-                        状态: ${if (res["status"] == "true") "在线" else "离线"}
-                    """.trimIndent()
-                    )
-                }
-
-            }
-        })
-    }
-
-    fun getStatus(group: Group,serverName:String) {
+    fun getStatus(group: Group, serverName: String = config.ServerName) {
 
         val request = Request.Builder()
             .url("${config.McsmIP}api/status/${serverName}")
@@ -57,13 +28,13 @@ object McsmRequests {
 
                 val res = Gson().fromJson(response.body?.string(), hashMapOf<String, String>().javaClass)
                 runBlocking {
-                    println(res.toString())
+                    McsmBot.logger.info { res.toString() }
                     group.sendMessage(
                         """
                         名称: ${res["id"]}
                         版本: ${res["version"]}
                         在线人数: ${res["current_players"]}/${res["max_players"]}
-                        状态: ${if (res["status"] == "true") "在线" else "离线"}
+                        状态: ${if (res["status"].toString() == "true") "在线" else "离线"}
                     """.trimIndent()
                     )
                 }
